ad790cb9c5e13c8379b7b2c67d5ad14dd67211f0
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2023-07-04 22:12:33 +0800

    move get_result_aw into a separate file

--- include/koios/task.h
+++ include/koios/task.h
@@ -7,9 +7,9 @@
 #include "koios/promise_base.h"
 #include "koios/promise_wrapper.h"
 #include "koios/return_value_or_void.h"
+#include "koios/from_result_aw.h"
 #include "koios/task_scheduler_wrapper.h"
-#include "koios/local_thread_scheduler.h"
-#include "koios/task_scheduler_selector.h"
+#include "koios/get_value_aw.h"
 
 KOIOS_NAMESPACE_BEG
 
@@ -19,73 +19,15 @@ struct _task
     struct [[nodiscard]] _type;
 };
 
-template<typename T, typename Task>
-class get_result_aw 
-    : public virtual task_scheduler_selector
-{
-public:
-    using value_type = T;
-
-    get_result_aw(promise_wrapper<value_type> promise)
-        : m_future{ promise.get_future() }, 
-          m_promise{ ::std::move(promise) }
-    {
-    }
-    
-    constexpr bool await_ready() const noexcept { return false; }
-    void await_suspend(::std::coroutine_handle<> h)
-    {
-        m_promise.set_caller(h);
-        scheduler().enqueue(
-            static_cast<Task*>(this)->move_out_coro_handle()
-        );
-    }
-
-    decltype(auto) await_resume() noexcept { return m_future.get(); }
-
-    auto& future() noexcept { return m_future; }
-
-protected:
-    ::std::future<T> m_future;
-    promise_wrapper<value_type> m_promise;
-};
-
-template<typename Task>
-class get_result_aw<void, Task>
-    : public virtual task_scheduler_selector
-{
-public:
-    get_result_aw(promise_wrapper<void> promise)
-        : m_future{ promise.get_future() }, 
-          m_promise{ ::std::move(promise) }
-    {
-    }
-
-    constexpr bool await_ready() const { return false; }
-    void await_suspend(::std::coroutine_handle<> h)
-    {
-        m_promise.set_caller(h);
-        scheduler().enqueue(
-            static_cast<Task*>(this)->move_out_coro_handle()
-        );
-    }
-
-    constexpr void await_resume() const noexcept {}
-
-    auto& future() noexcept { return m_future; }
-
-protected:
-    ::std::future<void> m_future;
-    promise_wrapper<void> m_promise;
-};
-
 template<typename T>
 class _task<T>::_type : public get_result_aw<T, _task<T>::_type>
 {
 public:
     using value_type = T;
 
-    class promise_type : public promise_base, public return_value_or_void<T, promise_type>
+    class promise_type 
+        : public promise_base, 
+          public return_value_or_void<T, promise_type>
     {
     public:
         _task<T>::_type get_return_object() noexcept

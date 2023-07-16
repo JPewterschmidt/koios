05dd8e258c7496c3d0c2bb43eae8dff3b6bc8bd1
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2023-07-16 22:54:01 +0800

    add allocator supports to generator

--- example/main.cc
+++ example/main.cc
@@ -6,7 +6,7 @@
 #include "glog/logging.h"
 
 #include "koios/task.h"
-#include "koios/expected_task.h"
+#include "koios/monad_task.h"
 #include "koios/thread_pool.h"
 #include "koios/from_result.h"
 #include "koios/generator.h"
@@ -22,60 +22,26 @@
 
 using namespace koios;
 
-namespace 
+struct foo
 {
-    int result{};
-    int count{};
-    int flag{};
-    int referd_obj{};
+    foo() = default;
+    foo(const foo&) { ::std::cout << "copy\n"; }
+    foo(foo&&) { ::std::cout << "move\n"; }
+};
 
-    ::std::binary_semaphore sem{0}; 
+::std::vector<foo> fvec(10);
+koios::generator<foo> g1()
+{
+    for (auto& f : fvec)
+        co_yield ::std::move(f);
+}
 
-    task<int> for_with_scheduler()
+int main()
+{
+    auto g = g1();
+    for (const auto& f : g)
     {
-        if (++count >= 10) co_return 1;
-        co_return co_await for_with_scheduler() + 1;
+        ::std::cout << "ok" << ::std::endl;
     }
 
-    task<void> starter()
-    {
-        result = co_await for_with_scheduler();
-        sem.release();
-    }
-
-    async_task<void> func1() { co_return; }
-    sync_task<void> func2() { co_return; }
-}
-
-task<void> for_basic_test()
-{
-    flag = 1;
-    co_return;
-}
-
-nodiscard_task<int> for_basic_test2()
-{
-    unsigned char buffer[1024];
-    co_return 2;
-}
-
-task<int&> for_basic_test3()
-{
-    co_return referd_obj;
-}
-
-monad_task<int> for_et()
-{
-    co_return 1;
-}
-
-int main(int argc, char** argv)
-{
-    auto result = for_et()
-        .transform([](int i) -> monad_task<int>{
-                co_return ::std::error_code{ 1,::std::system_category() }; 
-        }).transform([](int i) -> monad_task<int>{ 
-            co_return i * 100; 
-        }).run_and_get_future().get().get_value();
-    ::std::cout << result;
 }
--- include/koios/monad_task.h
+++ include/koios/monad_task.h
@@ -6,48 +6,17 @@
 #include <variant>
 #include <concepts>
 #include <system_error>
+#include <iostream> // only for a temporarily logging
 
 #include "koios/macros.h"
 #include "koios/task.h"
+#include "koios/expected_result.h"
+
 #include "toolpex/is_specialization_of.h"
 #include "toolpex/exceptions.h"
 
+
 KOIOS_NAMESPACE_BEG
-
-template<typename T, ::std::default_initializable E>
-class expected_result
-{
-private:
-    template<typename, ::std::default_initializable, driver_policy_concept, typename> 
-    friend class monad_task;
-
-    expected_result() = default;
-
-public:
-    using value_type = T;
-    using error_type = E;
-    using value_pointer = ::std::unique_ptr<value_type>;
-
-    expected_result(value_type val)
-        : m_result{ ::std::make_unique<value_type>(::std::move(val)) }
-    {
-    }
-
-    explicit expected_result(error_type e)
-        : m_error{ ::std::move(e) }
-    {
-    }
-
-    const error_type& error() const noexcept { return m_error; }
-    bool has_value() const noexcept { return m_result != nullptr; }
-    operator bool() const noexcept { return has_value(); }
-    value_pointer& value_storage() noexcept { return m_result; }
-    value_type get_value() { return ::std::move(*value_storage()); }
-    
-private:
-    value_pointer m_result;
-    error_type m_error{};
-};
 
 template<
     typename T, 
@@ -83,11 +52,13 @@ public:
     monad_task(promise_type& p)
         : base_type{ p }
     {
+        ::std::cout << "the monad task functionality now is not implemented!" << ::std::endl;
     }
 
     monad_task(base_type&& t)
         : base_type{ ::std::move(t) }
     {
+        ::std::cout << "the monad task functionality now is not implemented!" << ::std::endl;
     }
 
     monad_task(monad_task&& other) noexcept = default;

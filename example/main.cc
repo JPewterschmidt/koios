4e11e958855a6883cd77883690d428fec5092709
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2023-07-19 23:22:55 +0800

    update README

--- example/main.cc
+++ example/main.cc
@@ -1,59 +1,30 @@
-#include <vector>
-#include <functional>
-
-#include "fmt/core.h"
-#include "fmt/ranges.h"
-
-#include "koios/task.h"
-#include "koios/monad_task.h"
-#include "koios/thread_pool.h"
-#include "koios/from_result.h"
-#include "koios/generator.h"
-#include "koios/invocable_queue_wrapper.h"
-
-#include "toolpex/unique_resource.h"
-
-#include <chrono>
-#include <thread>
 #include <iostream>
-#include <future>
-#include <ranges>
-#include <iterator>
+#include "koios/task.h"
 
-using namespace ::std::chrono_literals;
-using namespace koios;
-
-namespace
+koios::task<void> coro_task3()
 {
-    int basic_count{};
-    int dtor_count{};
+    ::std::cout << "world!" << ::std::endl;
+    co_return;
 }
 
-using namespace koios;
-
-void test1()
+koios::task<int> coro_task2()
 {
-    dtor_count = 0;
-    {
-        koios::invocable_queue_wrapper iqw{ std_queue_wrapper{} };
+    ::std::cout << "koios' ";
+    co_await coro_task3();
+    co_return 1;
+}
 
-        for (size_t i{}; i < 10; ++i)
-            iqw.enqueue([h = toolpex::unique_resource(1, [](int){ ++dtor_count; })]{ ++basic_count; });
-
-        koios::invocable_queue_wrapper iqw2{ ::std::move(iqw) };
-
-        for (size_t i{}; i < 5; ++i)
-        {
-            auto ret = iqw2.dequeue();
-            if (ret) (*ret)();
-        }
-
-    }
-    ::std::cout << ::std::boolalpha << (dtor_count == 10) << ::std::endl;
+koios::task<int> coro_task()
+{
+    ::std::cout << "hello ";
+    co_return co_await coro_task2();
 }
 
 int main()
 {
-    test1();
-    return runtime_exit();
+    koios::runtime_init(12);
+
+    int val = coro_task().run_and_get_future().get();
+
+    return koios::runtime_exit();
 }

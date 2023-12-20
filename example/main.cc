6cbae15df8225a0e20012b14abd7150f7a565b2a
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2023-12-20 18:24:40 +0800

    add future/promise of koios

--- example/main.cc
+++ example/main.cc
@@ -1,54 +1,18 @@
-#include <iostream>
-#include "koios/task.h"
-#include "koios/event_loop.h"
-#include "toolpex/tic_toc.h"
-#include "koios/timer.h"
-#include "koios/this_task.h"
-#include "koios/generator.h"
-#include "koios/delayed_scheduler.h"
+#include "koios/future.h"
 
-#include <chrono>
-#include <algorithm>
-#include <numeric>
-#include <coroutine>
-
-using namespace koios;
-using namespace toolpex;
-using namespace ::std::chrono_literals;
-
-::std::binary_semaphore bs{0};
-
-task<void> func()
+void func(koios::promise<int> i)
 {
-    throw ::std::runtime_error{""};
-    co_return;
+    i.set_value(1);
+    i.send();
 }
-
-task<void> receiver()
-{
-    try
-    {
-        co_await func();
-    }
-    catch (...)
-    {
-        ::std::cout << "ok" << ::std::endl;
-    }
-
-    co_return;
-}
-
-#include "koios/event_loop_concepts.h"
 
 int main()
-try 
 {
-    ::std::cout << koios::event_loop_concept<timer_event_loop> << ::std::endl;
-
-    return 0;
-} 
-catch (const ::std::exception& e) 
-{
-    koios::log_error(e.what());
-    return 1;
+    koios::promise<int> ip{};
+    auto f = ip.get_future();
+    func(::std::move(ip));
+    if (f.ready())
+    {
+        ::std::cout << f.get() << ::std::endl;
+    }
 }

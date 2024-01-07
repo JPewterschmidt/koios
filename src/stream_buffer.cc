9e3360853ab688593ec838f9fc17c5e3f348c22f
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-01-07 11:52:56 +0800

    remove all things about recv_until

--- src/stream_buffer.cc
+++
@@ -1,6 +0,0 @@
-#include "koios/stream_buffer.h"
-#include <cstddef>
-
-//template class koios::stream_buffer<::std::byte>;
-template class koios::stream_buffer<unsigned char>;
-template class koios::stream_buffer<char>;

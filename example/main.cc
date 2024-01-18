23231587d0f132402086b822f3cb48a60e8a78f7
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-01-18 23:16:24 +0800

    add test of tcp_server

--- example/main.cc
+++ example/main.cc
@@ -29,71 +29,10 @@
 using namespace koios;
 using namespace ::std::chrono_literals;
 
-tcp_server* sp{};
-
-task<void> tcp_server_app(toolpex::unique_posix_fd client)
-{
-    static size_t count{};
-    auto [addr, port] = toolpex::ip_address::getpeername(client);
-    ::std::cout << "client: [addr: " << addr->to_string() << ", port: " << port << "]";
-
-    ::std::string msg = "fuck you!!!!";
-
-    ::std::array<char, 128> buffer{};
-
-    auto recv_ret = co_await uring::recv(client, buffer);
-    ::std::cout << recv_ret.nbytes_delivered() << "bytes delivered!" << ::std::endl;
-
-    co_await uring::send(client, msg);
-    ::std::string_view sv{ buffer.data(), recv_ret.nbytes_delivered() };
-    if (sv.contains("stop") && sp && ++count > 10)
-        sp->stop();
-
-    co_return;
-}
-
-task<void> client_app()
-{
-    using namespace toolpex::ip_address_literals;
-    using namespace ::std::string_view_literals;
-
-    auto sock = co_await uring::connect_get_sock("::1"_ip, 8889);
-    auto ret = co_await uring::send(sock, "fuck you"sv);
-    while (ret.error_code())
-    {
-        co_await uring::send(sock, "fuck you"sv);
-    }
-}
-
-expected_task<int, ::std::error_code> exp1(int i = 1)
-{
-    co_return i + 1;
-}
-
-expected_task<int, ::std::error_code> exp2(int i = 2)
-{
-    co_return unexpected_t<::std::error_code>{
-        ::std::error_code{ EINVAL, ::std::system_category() }
-    };
-}
-
-task<void> expected_emitter()
-{
-    auto e1 = co_await exp1();
-    auto e2 = co_await e1.and_then(exp1);
-    auto e3 = co_await e2.and_then(exp1);
-    auto e4 = co_await e3.and_then(exp1);
-
-    if (e4.has_value())
-    {
-        ::std::cout << "val: " << e4.value() << ::std::endl;
-    }
-
-    co_return;
-}
-
 task<void> emitter()
 {
+    
+
     co_return;
 }
 

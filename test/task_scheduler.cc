9227088f88dac25b59045ec7f4494cf4d68412d2
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-01-24 21:07:25 +0800

    optimize

--- test/task_scheduler.cc
+++ test/task_scheduler.cc
@@ -37,7 +37,7 @@ task<void> starter()
 
 TEST(task_scheduler, basic)
 {
-    constexpr size_t loop_size{ 10 };
+    constexpr size_t loop_size{ 100 };
     ::std::vector<koios::future<void>> fvec{};
     for (size_t i{}; i < loop_size; ++i)
         fvec.emplace_back(starter().run_and_get_future());

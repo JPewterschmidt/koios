c7192b907d187bc0312f0afb46e9ebd04be4656b
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-01-28 18:36:50 +0800

    add get_handle_aw, iouring_cancel_aw

---
+++ src/get_handle_aw.cc
@@ -0,0 +1,12 @@
+#include "koios/get_handle_aw.h"
+#include "koios/runtime.h"
+
+KOIOS_NAMESPACE_BEG
+
+void get_handle_aw::await_suspend(task_on_the_fly h) noexcept
+{
+    m_result = h.address();
+    get_task_scheduler().enqueue(::std::move(h));
+}
+
+KOIOS_NAMESPACE_END

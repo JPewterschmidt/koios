f82f4da59a698cc0cf73067ce2641bb95d9a9f55
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-04-18 20:58:28 +0800

    add posix_result_aw

--- src/iouring_single_op_aw.cc
+++ src/iouring_single_op_aw.cc
@@ -21,13 +21,14 @@
 namespace koios::uring
 {
 
-read_write_aw::read_write_aw()  noexcept : op_batch_execute_aw{ batch().rep() } { }
-socket_aw::socket_aw()          noexcept : op_batch_execute_aw{ batch().rep() } { }
-accept_aw::accept_aw()          noexcept : op_batch_execute_aw{ batch().rep() } { }
-connect_aw::connect_aw()        noexcept : op_batch_execute_aw{ batch().rep() } { }
-cancel_aw::cancel_aw()          noexcept : op_batch_execute_aw{ batch().rep() } { }
-fsync_aw::fsync_aw()            noexcept : op_batch_execute_aw{ batch().rep() } { }
-normal_aw::normal_aw()          noexcept : op_batch_execute_aw{ batch().rep() } { }
-nop_aw::nop_aw()                noexcept : op_batch_execute_aw{ batch().rep() } { }
+read_write_aw::read_write_aw()      noexcept : op_batch_execute_aw{ batch().rep() } { }
+socket_aw::socket_aw()              noexcept : op_batch_execute_aw{ batch().rep() } { }
+accept_aw::accept_aw()              noexcept : op_batch_execute_aw{ batch().rep() } { }
+connect_aw::connect_aw()            noexcept : op_batch_execute_aw{ batch().rep() } { }
+cancel_aw::cancel_aw()              noexcept : op_batch_execute_aw{ batch().rep() } { }
+fsync_aw::fsync_aw()                noexcept : op_batch_execute_aw{ batch().rep() } { }
+normal_aw::normal_aw()              noexcept : op_batch_execute_aw{ batch().rep() } { }
+nop_aw::nop_aw()                    noexcept : op_batch_execute_aw{ batch().rep() } { }
+posix_result_aw::posix_result_aw()  noexcept : op_batch_execute_aw{ batch().rep() } { }
 
 } // namespace koios::uring

0e3f8231245c45b31da07c2076576ef848ffffd7
Jeremy Pewterschmidt (Shixin Wang) <jeremypewterschmidt@gmail.com>
2024-11-06 16:43:32 +0800

    optimize coroutine_mutex

--- src/coroutine_mutex.cc
+++ src/coroutine_mutex.cc
@@ -6,51 +6,24 @@
 #include "koios/coroutine_mutex.h"
 #include "koios/runtime.h"
 #include "koios/exceptions.h"
-#include <mutex>
 
 KOIOS_NAMESPACE_BEG
-
-bool mutex::
-hold_this_immediately() noexcept
-{
-    ::std::lock_guard lk{ m_lock };
-    return m_holded ? false : (m_holded = true);
-}
-
-void mutex::
-add_waiting(task_on_the_fly h)
-{
-    m_waitings.enqueue({ .task = ::std::move(h) });
-}
-
-void mutex::
-try_wake_up_next_impl() noexcept
-{
-    if (being_held_impl()) return;
-
-    waiting_handle handle{};
-    if (m_waitings.try_dequeue(handle))
-    {
-        m_holded = true;
-        wake_up(handle);
-    }
-    else m_holded = false;
-}
 
 void mutex::
 try_wake_up_next() noexcept
 {
-    ::std::lock_guard lk{ m_lock };
-    this->try_wake_up_next_impl();
-}
+    if (!hold_this_immediately())
+        return;
 
-void
-mutex::
-release()
-{
-    ::std::lock_guard lk{ m_lock };
-    m_holded = false;
-    this->try_wake_up_next_impl();
+    waiting_handle handle{};
+    if (m_waitings.try_dequeue(handle))
+    {
+        wake_up(handle);
+    }
+    else
+    {
+        m_flag.clear(::std::memory_order_release);
+    }
 }
 
 KOIOS_NAMESPACE_END
